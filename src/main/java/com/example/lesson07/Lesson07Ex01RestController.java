05de42aeecc140b66bd6c660af33de7c2383d22d
3oreum <143936380+3oreum@users.noreply.github.com>
2023-10-22 19:47:28 +0900

    jpa

---
+++ src/main/java/com/example/lesson07/Lesson07Ex01RestController.java
@@ -0,0 +1,31 @@
+package com.example.lesson07;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.lesson04.bo.StudentBO;
+import com.example.lesson07.entity.StudentEntity;
+
+@RequestMapping("/lesson07/ex01")
+@RestController
+public class Lesson07Ex01RestController {
+	
+	@Autowired
+	private StudentBO studentBO;
+	
+	// C U D
+	
+	// C: create, insert
+	@GetMapping("/1")
+	public StudentEntity create() {
+		String name = "스폰지밥";
+		String phoneNumber = "010-5555-0000";
+		String email = "nemo@baji.com";
+		String dreamJob ="집게리아사장";
+		
+		// 지금 들어간 id 값도 바로 꺼낼 수 있다. 게터로. getId(); 
+		return studentBO.addStudent(name, phoneNumber, email, dreamJob);
+	}
+}
