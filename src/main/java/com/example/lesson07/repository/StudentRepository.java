a4a94854638f9a16713e5fd40fe91a87680f4647
학원 <test@test.com>
2023-10-24 21:41:14 +0900

    jpa

--- src/main/java/com/example/lesson07/repository/StudentRepository.java
+++ src/main/java/com/example/lesson07/repository/StudentRepository.java
@@ -30,7 +30,10 @@ public interface StudentRepository extends JpaRepository<StudentEntity, Integer>
 	
 	// ex02/2
 	// 1) JPQL 디비로 안 가고 엔티티로 조회
-	@Query(value = "select st from new_student st where st.dreamJob = :dreamJob")
+	//@Query(value = "select st from new_student st where st.dreamJob = :dreamJob")
+	
+	// 2) native query로 조회
+	@Query(value = "select * from `new_student` where dreamJob = :dreamJob", nativeQuery = true)
 	public List<StudentEntity> findByDreamJob(@Param("dreamJob") String dreamJob);
 	
 }
